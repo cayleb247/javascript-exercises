396a10f859cb4a38e2fa38db46ff6837d6a41bb0
cayleb247 <156046197+cayleb247@users.noreply.github.com>
2024-07-26 00:49:35 -0400

    Make function which multiplies a string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num == 0 || num < 0) {
+        if (num < 0) {
+            return 'ERROR';
+        } else {
+            return '';
+        }
+    }
+    let output = string;
 
+    for (num; num > 1; num--) {
+        output += string;
+    }
+
+    return output;
 };
 
 // Do not edit below this line
